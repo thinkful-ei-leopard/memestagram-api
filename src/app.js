10ce5bdce145909bec672ebb6cd348561a38b3e3
Lillian Burnside <lillian.burnside@gmail.com>
2020-04-22 11:40:54 -0500

    some router changes

--- src/app.js
+++ src/app.js
@@ -15,11 +15,7 @@ const morganOption = (NODE_ENV === 'production')
     app.use(morgan(morganOption))
     app.use(helmet())
     app.use(cors())
-
-    app.get('/', (req, res) => {
-        res.send('Hello, world!')
-    })
-
+    
     app.post('/imageupload', imageUploadRouter)
 
     app.use(function errorHandler(error, req, res, next) {
--- src/image-upload/image-upload-router.js
+++ src/image-upload/image-upload-router.js
@@ -1,23 +1,39 @@
-require('dotenv').config()
-const cloudinary = require('cloudinary')
-const formData = require('express-form-data')
-const { CLIENT_ORIGIN } = require('./config')
+'use strict';
+require('dotenv').config();
+const cloudinary = require('cloudinary');
+const express = require('express');
+const formData = require('express-form-data');
+const ImageUploadService = require('./image-upload-service');
+//const { CLIENT_ORIGIN } = require('./config');
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET
-})
+});
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
 authRouter
-  .route('/')
-  .post(formData.parse(), (req, res) => {
-    const values = Object.values(req.files)
-    const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  .route('/signup')
+  .post(formData.parse(), (req, res, next) => {
+    const values = Object.values(req.files);
+    const promises = values.map(image => cloudinary.uploader.upload(image.path));
 
+    // ImageUploadService.insertImage(req.app.get('db'), newImage)
     Promise
-    .all(promises)
-    .then(results => res.json(results))
-  })
+      .all(promises)
+      .then(results => res.json(results));
+  });
+
+// POST the photo when creating a new post
+// authRouter
+//   .route('/photo')
+//   .post(formData.parse(), (req, res) => {
+//     const values = Object.values(req.files);
+//     const promises = values.map(image => cloudinary.uploader.upload(image.path));
+
+//     Promise
+//       .all(promises)
+//       .then(results => res.json(results));
+//   });
