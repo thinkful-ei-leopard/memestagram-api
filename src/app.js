3d9ea9333c48492956559a1a2ecc9fe5ec5b1642
Hsin Ling Hu <hsinkava11@gmail.com>
2020-04-22 23:22:51 -0700

    add both posts and comment table service/router

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const postsRouter = require('./posts/posts-router');
 
-const app = express ()
+const app = express ();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+  res.send('Hello, world!');
+});
+
+app.use('/api/post', postsRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response
