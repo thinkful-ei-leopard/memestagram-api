0522216c7b035340cc082fba6a10a44016215e03
Hsin Ling Hu <hsinkava11@gmail.com>
2020-05-10 16:45:11 -0700

    cleanTables error

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -1,12 +1,105 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+
+describe('Auth Endpoints', function(){
+  let db;
+
+  const { testUsers } = helpers.makePostsFixtures();
+  const testUser = testUsers[0];
+
+  before('make knex instance', () =>{
+    db = helpers.makeKnexInstance();
+    
+    app.set('db', db);
+  });
+
+  after('disconnect from db', ()=> db.destroy());
+  before('cleanup', () => helpers.cleanTables(db));
+  afterEach('cleanup', () => helpers.cleanTables(db));
+
+  describe('POST/api/auth/login', ()=>{
+    beforeEach('insert user', () =>
+      helpers.seedUsers(
+        db,
+        testUsers
+      )
+    );
+    const requireFields = ['user_name', 'password'];
+
+    requireFields.forEach(field =>{
+      const loginAttemptBody ={
+        user_name:testUser.user_name,
+        password:testUser.password
+      };
+
+      it(`responds with 400 required error when '${field}' is missing`, ()=> {
+        delete loginAttemptBody [field];
+
+        return supertest(app)
+          .post('/api/auth/login')
+          .send(loginAttemptBody)
+          .expect(400, {
+            error:`Missing ${field} in request body`,
+          });
+      });
+
+    });
+    it(`'responds 400 'invalid user_name or password' when bad user_name'`,()=> {
+      const userInvalidUser ={ username:'user-not', password:'existy'};
+      return supertest(app)
+        .post('/api/auth/login')
+        .send(userInvalidUser)
+        .expect(400, {
+          error:'Incorrect user_name or password'
+        });
+    });
+    it(`responds 400 'invalid user_name or password' when bad user_name`,()=> {
+      const userInvalidUser ={ username:'testUser.user_name', password:'incorrect'};
+      return supertest(app)
+        .post('/api/auth/login')
+        .send(userInvalidUser)
+        .expect(400, {
+          error:'Incorrect user_name or password'
+        });
+    });
+    it(`responds 200 and JWT auth token using secret when valid credentials`, ()=>{
+      
+      const userValidCreds={
+        username: testUser.user_name,
+        password: testUser.password
+      };
+
+      const expectedToken =jwt.sign(
+        { user_id: testUser.id},
+        process.env.JWT_SECRET,
+        {
+          subject:testUser.user_name,
+          algorithm:'HS256'
+        }
+      );
+      return supertest(app)
+        .post('/api/auth/login')
+        .send(userValidCreds)
+        .expect(200, {
+          authToken:expectedToken,
+          user_id:testUser.id
+        });
+    });
+    
+  });
+});
+
+
+/*const jwt = require('jsonwebtoken')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
 describe('Auth Endpoints', function () {
-  let db
+  let db;
 
-  const testUsers = helpers.makeUsersArray()
-  const testUser = testUsers[0]
+  const {testUsers} = helpers.makePostsFixtures()
+  const testUser = testUsers[0];
 
   before('make knex instance', () => {
     db = helpers.makeKnexInstance()
@@ -19,10 +112,8 @@ describe('Auth Endpoints', function () {
 
   afterEach('cleanup', () => helpers.cleanTables(db))
 
-  /**
-   * @description Get token for login
-   **/
-  describe(`POST /api/auth/token`, () => {
+  
+  describe(`POST /api/auth/login`, () => {
     beforeEach('insert users', () =>
       helpers.seedUsers(
         db,
@@ -92,7 +183,7 @@ describe('Auth Endpoints', function () {
   /**
    * @description Refresh token
    **/
-  describe(`PATCH /api/auth/token`, () => {
+ /* describe(`PATCH /api/auth/token`, () => {
     beforeEach('insert users', () =>
       helpers.seedUsers(
         db,
@@ -118,4 +209,5 @@ describe('Auth Endpoints', function () {
         })
     })
   })
-})
+})*/
+
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,15 @@
-const { expect } = require('chai')
-const supertest = require('supertest')
+process.env.TZ = 'UCT';
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'test-jwt-secret';
+process.env.JWT_EXPIRY = '3m';
 
-global.expect = expect
-global.supertest = supertest
+require('dotenv').config();
+
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
+  || "postgresql://dunder_mifflin@localhost/memestagram-test";
+
+const { expect } = require('chai');
+const supertest = require('supertest');
+
+global.expect = expect;
+global.supertest = supertest;
