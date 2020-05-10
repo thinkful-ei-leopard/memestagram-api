30479736239457d44ad059959dd576742956b2b5
shimmy77 <christianshim@gmail.com>
2020-05-10 15:05:12 -0500

    testing fixing

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -2,10 +2,7 @@ const knex = require('knex')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-/**
- * create a knex instance connected to postgres
- * @returns {knex instance}
- */
+
 function makeKnexInstance() {
   return knex({
     client: 'pg',
@@ -13,10 +10,6 @@ function makeKnexInstance() {
   })
 }
 
-/**
- * create a knex instance connected to postgres
- * @returns {array} of user objects
- */
 function makeUsersArray() {
   return [
     {
@@ -34,12 +27,30 @@ function makeUsersArray() {
   ]
 }
 
-/**
- * make a bearer token with jwt for authorization header
- * @param {object} user - contains `id`, `username`
- * @param {string} secret - used to create the JWT
- * @returns {string} - for HTTP authorization header
- */
+function makePostsArray() {
+  return [
+    {
+      id: 1,
+      memeImg: data.memeImg,
+      description: 'test description',
+      likes: '111',
+      user_id: 2
+    }
+  ]
+}
+
+function makeCommentArray() {
+  return [
+    {
+      id: 1,
+      comment: 'test comment',
+      username: 'test username',
+      posts_id: 2,
+      user_id: 3
+    }
+  ]
+}
+
 function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
     const token = jwt.sign({ user_id: user.id }, secret, {
       subject: user.username,
@@ -48,18 +59,6 @@ function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
     return `Bearer ${token}`
   }
 
-  /**
- * insert users into db with bcrypted passwords and update sequence
- * @param {knex instance} db
- * @param {array} users - array of user objects for insertion
- * @returns {Promise} - when users table seeded
- */
-
- /**
- * remove data from tables and reset sequences for SERIAL id fields
- * @param {knex instance} db
- * @returns {Promise} - when tables are cleared
- */
 function cleanTables(db) {
   return db.transaction(trx =>
     trx.raw(
@@ -72,6 +71,23 @@ function cleanTables(db) {
       )
 }
 
+function seedPost(db, posts) {
+    const newPosts = posts.map(post => ({
+      ...post,
+    }))
+    return db.transaction(async trx => {
+      await trx.into('post').insert(newPosts)
+    })
+}
+
+function seedComment(db, comment) {
+  const newComment = comment.map(comment => ({
+    ...comment,
+  }))
+  return db.transaction(async trx => {
+    await trx.into('comment').insert(newComment)
+  })
+}
 
 function seedUsers(db, users) {
     const preppedUsers = users.map(user => ({
@@ -92,6 +108,10 @@ function seedUsers(db, users) {
     makeKnexInstance,
     makeUsersArray,
     makeAuthHeader,
+    makePostsArray,
+    makeCommentArray,
     cleanTables,
+    seedPost,
     seedUsers,
+    seedComment
   }
