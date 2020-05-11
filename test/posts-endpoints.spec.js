43d9914f96ccce807b896e1d9afbff4265ea5480
Lillian Burnside <lillian.burnside@gmail.com>
2020-05-11 18:14:08 -0500

    posts get endpoint

--- test/posts-endpoints.spec.js
+++ test/posts-endpoints.spec.js
@@ -1,8 +1,9 @@
-const knex = require('knex')
-const app = require('../src/app')
-const helpers = require('./test-helpers')
+'use strict';
+const knex = require('knex');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
 
-describe ('Post Endpoints', function () {
+describe.only('Posts Endpoints', function () {
   let db;
 
   before('make knex instance', () => {
@@ -13,13 +14,13 @@ describe ('Post Endpoints', function () {
     app.set('db', db);
   });
 
-  after ('disconnect from db', () => db.destroy())
+  after ('disconnect from db', () => db.destroy());
   
-  before('cleanup', () => helpers.cleanTables(db))
+  before('cleanup', () => helpers.cleanTables(db));
   
-  afterEach('cleanup', () => helpers.cleanTables(db))
+  afterEach('cleanup', () => helpers.cleanTables(db));
 
-  describe(`GET /api/`, () => {
+  describe.only('GET /api/posts', () => {
     const testUser = {
       id: 1,
       username: 'test-user-1',
@@ -28,13 +29,43 @@ describe ('Post Endpoints', function () {
       userImg: 'testImg'
     };
 
-    const testPost = {
-      id: 1,
-      memeImg: 'testImg',
-      description: 'testDesc',
-      likes: 0,
-      user_id: 1
-    };
+    const testPosts = [
+      {
+        id: 1,
+        memeImg: 'testImg',
+        description: 'testDesc',
+        likes: 0,
+        user_id: 1
+      },
+      {
+        id: 2,
+        memeImg: 'testImg2',
+        description: 'testDesc2',
+        likes: 0,
+        user_id: 1
+      }
+    ];
+
+    const expectedPosts = [
+      {
+        id: 1,
+        memeImg: 'testImg',
+        description: 'testDesc',
+        likes: 0,
+        user_id: 1,
+        userImg: 'testImg',
+        username: 'test-user-1'
+      },
+      {
+        id: 2,
+        memeImg: 'testImg2',
+        description: 'testDesc2',
+        likes: 0,
+        user_id: 1,
+        userImg: 'testImg',
+        username: 'test-user-1'
+      }
+    ];
 
     beforeEach('insert users and posts', () => {
       return db 
@@ -43,11 +74,16 @@ describe ('Post Endpoints', function () {
         .then(() => {
           return db 
             .into('posts')
-            .insert(testPost);
-        })
+            .insert(testPosts);
+        });
     });
 
-  });
+    it('responds with a 200 and all the posts', () => {
+      return supertest(app)
+        .get('/api/posts')
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .expect(200, expectedPosts);
+    });
 
   });
 
@@ -78,6 +114,7 @@ describe ('Post Endpoints', function () {
             .insert(testPost);
         });
     });
-  })
+  });
+});
 
 
