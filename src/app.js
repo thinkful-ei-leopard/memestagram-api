bf348d56f44b1a8482f2568d1fd7cb7edcb643ab
Hsin Ling Hu <hsinkava11@gmail.com>
2020-05-20 12:40:06 -0700

    revised app.js

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({origin: CLIENT_ORIGIN}));
 app.use(bodyParser.json());
 
 app.use('/api/comments', commentsRouter);
